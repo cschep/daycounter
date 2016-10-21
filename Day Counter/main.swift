05c33243c311bf1d4d5cd4d6670e1874b6664782
Chris Schepman <cschep@gmail.com>
2016-10-21 16:04:11 -0400

    Initial Commit

---
+++ Day Counter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day Counter
+//
+//  Created by Christopher Schepman on 10/21/16.
+//  Copyright © 2016 Christopher Schepman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
